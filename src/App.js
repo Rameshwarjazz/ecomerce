fceb479114e64c0f62d7271c6af0ac6684099212
Rameshwar <rjsahab143@gmail.com>
2023-10-29 18:15:12 +0530

    Added hero and some style

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
 import { Navbar } from './Component/Navbar/Navbar';
 import './App.css';
-
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import { Shop } from './Pages/Shop';
+import { ShopCategory } from './Pages/ShopCategory';
+import { LoginSignup } from './Pages/LoginSignup';
+import { Cart } from './Pages/Cart';
+import { Product } from './Pages/Product';
 function App() {
   return (
     <div>
-      <Navbar/>      
+      <BrowserRouter>
+      <Navbar/>
+      <Routes>
+        <Route path='/' element={<Shop/>}/>
+        <Route path='/mens' element={<ShopCategory category = "mens"/>}/>
+        <Route path='/womens' element={<ShopCategory category = "womens"/>}/>
+        <Route path='/kids' element={<ShopCategory category = "kids"/>}/>
+        <Route path='/product' element={<Product/>}>
+          <Route path='/productId' element={<Product/>}/>
+        </Route>
+        <Route path='/cart' element={<Cart/>}/>  
+        <Route path='/login' element={<LoginSignup/>}/>
+        
+      </Routes> 
+      </BrowserRouter>
+           
     </div>
   );
 }
